efc75e9cf78f8c378e59810ceaee3b8d30f97bf2
sergeyneuymin <sergeyneuymin27@gmail.com>
2023-06-15 15:49:36 +0500

    Spring doc integrated

--- src/main/java/org/neuimin/rugram/controller/UserController.java
+++ src/main/java/org/neuimin/rugram/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.neuimin.rugram.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.neuimin.rugram.entity.Subscription;
 import org.neuimin.rugram.entity.User;
 import org.neuimin.rugram.service.UserService;
@@ -22,26 +23,31 @@ public class UserController {
     // CRUD
 
     @PostMapping
+    @Operation(summary = "Adding new user")
     public String createUser(@RequestBody User user) {
         return service.createUser(user);
     }
 
     @GetMapping(path = "/{id}")
+    @Operation(summary = "Getting user")
     public User getUser(@PathVariable Long id) {
         return service.getUser(id);
     }
 
     @GetMapping
+    @Operation(summary = "Getting list of all users")
     public List<User> getUsers() {
         return service.getUsers();
     }
 
     @PutMapping(path = "/{id}")
+    @Operation(summary = "Updating user")
     public String updateUser(@RequestBody User user, @PathVariable Long id) {
         return service.updateUser(user, id);
     }
 
     @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Deleting user")
     public String deleteUser(@PathVariable Long id) {
         return service.deleteUser(id);
     }
@@ -49,11 +55,13 @@ public class UserController {
     // subscriptions
 
     @PostMapping(path = "/subscribe")
+    @Operation(summary = "Subscribe")
     public String subscribe(@RequestBody Subscription subscription) {
         return service.subscribe(subscription);
     }
 
     @DeleteMapping(path = "/unsubscribe/{subscriberId}/{subscriptionId}")
+    @Operation(summary = "Unsubscribe")
     public String unsubscribe(@PathVariable Long subscriberId, @PathVariable Long subscriptionId) {
         return service.unsubscribe(subscriberId, subscriptionId);
     }
